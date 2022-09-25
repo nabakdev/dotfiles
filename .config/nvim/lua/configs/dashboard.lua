040054d50d217ba938e29e5ce3dccc25a438c12d
nabakdev <almangox@gmail.com>
2022-09-25 14:59:46 +0700

    Add new plugins config

---
+++ .config/nvim/lua/configs/dashboard.lua
@@ -0,0 +1,58 @@
+local home = os.getenv('HOME')
+local db = require('dashboard')
+-- macos
+-- db.preview_command = 'cat | lolcat -F 0.3'
+-- linux
+-- db.preview_command = 'ueberzug'
+--
+-- db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
+--[[ let g:dashboard_custom_header9=[
+    \'',
+    \' ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ',
+    \' ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ',
+    \' ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ',
+    \' ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ',
+    \' ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ',
+    \' ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ',
+    \' ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ',
+    \' ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ',
+    \' ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ',
+    \' ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ',
+    \' ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ',
+    \' ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ',
+    \' ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ',
+    \' ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ',
+    \'',
+    \] ]]
+db.preview_file_height = 12
+db.preview_file_width = 80
+db.custom_center = {
+  {icon = '   ',
+  desc = 'Recently latest session                 ',
+  shortcut = 'SPC s l',
+  action ='SessionLoad'},
+  {icon = '  ',
+  desc = 'Recently opened files                   ',
+  action =  'DashboardFindHistory',
+  shortcut = 'SPC f h'},
+  {icon = '  ',
+  desc = 'Find  File                              ',
+  action = 'Telescope find_files find_command=rg,--hidden,--files',
+  shortcut = 'SPC f f'},
+  {icon = '  ',
+  desc ='File Browser                            ',
+  action =  'Telescope file_browser',
+  shortcut = 'SPC f b'},
+  {icon = '  ',
+  desc = 'Find  word                              ',
+  action = 'Telescope live_grep',
+  shortcut = 'SPC f g'},
+  {icon = '  ',
+  desc = 'Open Personal dotfiles                  ',
+  action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
+  shortcut = 'SPC f d'},
+  {icon = '  ',
+  desc = 'Update Plugins                          ',
+  action = 'PackerSync',
+  shortcut = 'SPC f u'},
+}
---
+++ .config/nvim/lua/configs/kommentary.lua
@@ -0,0 +1,4 @@
+require'kommentary.config'.configure_language('vue', {
+    single_line_comment_string = '//',
+})
+require'kommentary.config'.use_extended_mappings() 
