43cfe2e1d289f50053a87d5cd3b51e3cac92d159
nabakdev <almangox@gmail.com>
2023-01-30 12:44:45 +0700

    cleaning unused configs

--- .config/nvim/lua/configs/nvim-autopairs.lua
+++ .config/nvim/lua/configs/nvim-autopairs.lua
@@ -32,23 +32,6 @@ npairs.add_rules {
 }
 
 
--- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done())
-
-
--- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
--- cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
-
--- you need setup cmp first put this after cmp.setup()
---[[ require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-    tex = '{'
-  }
-}) ]]
