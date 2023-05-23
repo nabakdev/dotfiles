0095a61d4f3b5cb9d4afe52420196e78e7f8652a
nabakdev <almangox@gmail.com>
2023-05-23 18:52:39 +0700

    .

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local opt = { silent = true, noremap = true }
+local map = vim.api.nvim_set_keymap
+
+map("n", "<leader>fg", ':lua require("telescope.builtin").live_grep()<CR>', opt)
